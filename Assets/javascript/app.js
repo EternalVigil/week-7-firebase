60571872106a3cc6a3c003afc2c76e4e73c29c6d
EternalVigil <theeternalvigil@gmail.com>
2016-11-09 17:51:12 -0800

    edits to time compare algorithm computation

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -12,60 +12,60 @@
 
   database.ref().on("child_added", function (snapshot) {
   	"use strict";
-	//grabs values from Firebase to complete trainTable values
+  	//grabs values from Firebase to complete trainTable values
   	var newTrain = snapshot.child("name").val();
   	var newDestination = snapshot.child("destination").val();
   	var newFrequency = snapshot.child("frequency").val();
   	var nextArrival = null;
   	var minAway = null;
 
-	//grabs value from Firebase of starting time for train to calculate train schedule arrival times
-  	var newStart = snapshot.child("startTime").val();  
-	console.log("startTime from Database: " + newStart);
-	
-	//parse startTime value into moment.js format to convert into Unix milliseconds timecode based on daily schedules
-	var parseTime = newStart.split();
+  	//grabs value from Firebase of starting time for train to calculate train schedule arrival times
+  	var newStart = snapshot.child("startTime").val();
+  	console.log("startTime from Database: " + newStart);
+
+  	//parse startTime value into moment.js format to convert into Unix milliseconds timecode based on daily schedules
+  	var parseTime = newStart.split();
   	var todayHour = parseTime[0];
   	var todayMin = parseTime[1];
-	  
-	//converts frequency into milliseconds to create train interval time
-	var frequencyInterval = newFrequency * 60000;
-	var tempTime = moment({
+  	var tempTime = moment({
   		hour: todayHour,
   		minute: todayMin
   	});
-  	tempTime = moment(tempTime).unix();
+  	tempTime = moment.unix(tempTime / 1000000);
   	console.log("time from database: " + tempTime);
-	console.log("which translates to " + moment(tempTime).format("HH:mm"));
-	
-	//grabs current time and converts to Unix format
+  	console.log("which translates to " + moment(tempTime).format("HH:mm"));
+
+  	//converts frequency into milliseconds to create train interval time
+  	var frequencyInterval = newFrequency * 60;
+
+
+  	//grabs current time and converts to Unix format
   	var currentTime = moment();
-  	currentTime = moment(currentTime).unix();
+  	var currentTimeArray = currentTime.split(":");
+  	var currentHour = currentTimeArray[0];
+  	var currentMinutes = currentTimeArray[1];
+  	var currentTimeUnix = moment({
+  		hour: currentHour,
+  		minutes: currentMinutes
+  	}).unix();
   	console.log("current time: " + currentTime);
-	console.log("which translates to " + moment(currentTime).format("HH:mm"));
+  	console.log("which translates to " + moment(currentTime).format("HH:mm"));
 
-	//loop until tempTime <= current Time to calculate for time til next train
-  	do {
-  		tempTime += frequencyInterval;
-		console.log("the current time is " + moment(tempTime).format("HH:mm"))
+  	//loop until tempTime <= current Time to calculate for time til next train
+  	for (var i = 0; tempTime <= currentTimeUnix; i++) {
+  		tempTime = tempTime + frequencyInterval;
+  	}
+  	//calculates minutes left until next train arrives
+  	var timeDiff = (tempTime - currentTimeUnix) / 60;
+  	tempTime = moment.unix(tempTime).format("HH:mm");
 
-  	} while (tempTime < currentTime);
-	
-	var timeDiff = moment(tempTime).diff(currentTime);
-  	tempTime = moment(tempTime).format("HH:mm");
-  	//console.log(tempTime);
-	  timeDiff = moment(timeDiff).format("mm")
-	  console.log(timeDiff);
-	  nextArrival = tempTime;
-	  minAway = timeDiff;
-	  
-	//create entry string and appends to trainTable
+  	//create entry string and appends to trainTable
   	var entry = "<tr><td>" + newTrain + "</td><td>" + newDestination + "</td><td>" + newFrequency + "</td><td>" + nextArrival + "</td>" + "<td>" + minAway + "</td>";
   	$("#trainTable").append(entry);
 
   });
 
-//creates on click functionality to submit user input values to Firebase database
+  //creates on click functionality to submit user input values to Firebase database
   $("#submitButton").on("click", function () {
   	"use strict";
 
