2e7880d83874e3934a42d8a513bf624e7af38bac
Brandon <theeternalvigil@gmail.com>
2016-11-09 17:38:02 -0800

    added time compare functionality

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -12,42 +12,60 @@
 
   database.ref().on("child_added", function (snapshot) {
   	"use strict";
-  	//console.log(snapshot.child("name").val());
-
+	//grabs values from Firebase to complete trainTable values
   	var newTrain = snapshot.child("name").val();
   	var newDestination = snapshot.child("destination").val();
   	var newFrequency = snapshot.child("frequency").val();
   	var nextArrival = null;
   	var minAway = null;
 
-  	var newStart = snapshot.child("startTime").val();
-  	newStart = moment(newStart, "HH:mm");
-  	console.log(newStart);
-
+	//grabs value from Firebase of starting time for train to calculate train schedule arrival times
+  	var newStart = snapshot.child("startTime").val();  
+	console.log("startTime from Database: " + newStart);
+	
+	//parse startTime value into moment.js format to convert into Unix milliseconds timecode based on daily schedules
+	var parseTime = newStart.split();
+  	var todayHour = parseTime[0];
+  	var todayMin = parseTime[1];
+	  
+	//converts frequency into milliseconds to create train interval time
+	var frequencyInterval = newFrequency * 60000;
+	var tempTime = moment({
+  		hour: todayHour,
+  		minute: todayMin
+  	});
+  	tempTime = moment(tempTime).unix();
+  	console.log("time from database: " + tempTime);
+	console.log("which translates to " + moment(tempTime).format("HH:mm"));
+	
+	//grabs current time and converts to Unix format
   	var currentTime = moment();
-  	currentTime = moment.unix(currentTime);
-  	console.log(currentTime);
+  	currentTime = moment(currentTime).unix();
+  	console.log("current time: " + currentTime);
+	console.log("which translates to " + moment(currentTime).format("HH:mm"));
 
-  	var temp = newStart;
-
+	//loop until tempTime <= current Time to calculate for time til next train
   	do {
-  		temp += moment().add(newFrequency);
+  		tempTime += frequencyInterval;
+		console.log("the current time is " + moment(tempTime).format("HH:mm"))
 
-  	} while (temp <= currentTime);
-  	temp = moment(temp).format("HH:mm");
-  	console.log(temp);
-
-
-
-  	//var timeDifference = moment.diff(newStart);
-  	//console.log(timeDifference);
-
+  	} while (tempTime < currentTime);
+	
+	var timeDiff = moment(tempTime).diff(currentTime);
+  	tempTime = moment(tempTime).format("HH:mm");
+  	//console.log(tempTime);
+	  timeDiff = moment(timeDiff).format("mm")
+	  console.log(timeDiff);
+	  nextArrival = tempTime;
+	  minAway = timeDiff;
+	  
+	//create entry string and appends to trainTable
   	var entry = "<tr><td>" + newTrain + "</td><td>" + newDestination + "</td><td>" + newFrequency + "</td><td>" + nextArrival + "</td>" + "<td>" + minAway + "</td>";
-
   	$("#trainTable").append(entry);
 
   });
 
+//creates on click functionality to submit user input values to Firebase database
   $("#submitButton").on("click", function () {
   	"use strict";
 
