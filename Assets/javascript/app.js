f96ac20bcd7f4676582c23f7c69442765c57cc48
Brandon <theeternalvigil@gmail.com>
2016-11-10 14:16:29 -0800

    inital heroku commit

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -11,7 +11,6 @@
   var database = firebase.database();
 
   database.ref().on("child_added", function (snapshot) {
-  	"use strict";
   	//grabs values from Firebase to complete trainTable values
   	var newTrain = snapshot.child("name").val();
   	var newDestination = snapshot.child("destination").val();
@@ -21,10 +20,9 @@
 
   	//grabs value from Firebase of starting time for train to calculate train schedule arrival times
   	var newStart = snapshot.child("startTime").val();
-  	console.log("startTime from Database: " + newStart);
 
   	//parse startTime value into moment.js format to convert into Unix milliseconds timecode based on daily schedules
-  	var parseTime = newStart.split();
+  	var parseTime = newStart.split(":");
   	var todayHour = parseTime[0];
   	var todayMin = parseTime[1];
   	var tempTime = moment({
@@ -32,35 +30,27 @@
   		minute: todayMin
   	});
   	tempTime = moment.unix(tempTime / 1000000);
-  	console.log("time from database: " + tempTime);
-  	console.log("which translates to " + moment(tempTime).format("HH:mm"));
 
   	//converts frequency into milliseconds to create train interval time
   	var frequencyInterval = newFrequency * 60;
 
 
   	//grabs current time and converts to Unix format
-  	var currentTime = moment();
-  	var currentTimeArray = currentTime.split(":");
-  	var currentHour = currentTimeArray[0];
-  	var currentMinutes = currentTimeArray[1];
-  	var currentTimeUnix = moment({
-  		hour: currentHour,
-  		minutes: currentMinutes
-  	}).unix();
-  	console.log("current time: " + currentTime);
-  	console.log("which translates to " + moment(currentTime).format("HH:mm"));
+  	var currentTimeUnix = moment().unix();
+
 
   	//loop until tempTime <= current Time to calculate for time til next train
   	for (var i = 0; tempTime <= currentTimeUnix; i++) {
   		tempTime = tempTime + frequencyInterval;
   	}
+  	nextArrival = tempTime;
   	//calculates minutes left until next train arrives
   	var timeDiff = (tempTime - currentTimeUnix) / 60;
-  	tempTime = moment.unix(tempTime).format("HH:mm");
+  	timeDiff = parseInt(timeDiff);
+  	nextArrival = moment.unix(tempTime).format("HH:mm");
 
   	//create entry string and appends to trainTable
-  	var entry = "<tr><td>" + newTrain + "</td><td>" + newDestination + "</td><td>" + newFrequency + "</td><td>" + nextArrival + "</td>" + "<td>" + minAway + "</td>";
+  	var entry = "<tr><td>" + newTrain + "</td><td>" + newDestination + "</td><td>" + newFrequency + "</td><td>" + nextArrival + "</td>" + "<td>~" + timeDiff + "</td>";
   	$("#trainTable").append(entry);
 
   });
